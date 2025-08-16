503e887168261d4520844f8d7a459af1fa4cb9b8
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-08-16 19:43:45 -0300

    add utils

---
+++ src/utils/dateFormatter.ts
@@ -0,0 +1,17 @@
+export function formatDateToISO(dateStr: string): string {
+	if (dateStr.length !== 8 || isNaN(Number(dateStr))) {
+		throw new Error("Invalid date format: expected ddMMyyyy (ex: 10021999)");
+	}
+	const day = dateStr.slice(0, 2);
+	const month = dateStr.slice(2, 4);
+	const year = dateStr.slice(4, 8);
+	const isoDate = `${year}-${month}-${day}`;
+
+	// Validação básica: cria um Date para checar se é válido
+	const dateObj = new Date(isoDate);
+	if (isNaN(dateObj.getTime())) {
+		throw new Error("Invalid date value");
+	}
+
+	return isoDate;
+}
