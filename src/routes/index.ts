a05edd440cc4f2df329027f56fad0464a668ae30
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-08-16 22:01:52 -0300

    add auth route and fix users prefix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { userRoutes } from "../modules/user/user.route.ts";
+import { authRoutes } from "../modules/auth/auth.route.ts";
 
 export default async function (app: FastifyInstance) {
-	await app.register(userRoutes);
+	await app.register(userRoutes, { prefix: "/users" });
+	await app.register(authRoutes, { prefix: "/auth" });
 }
