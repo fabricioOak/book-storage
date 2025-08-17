cdc8238c3cdfdc530c713d71e7327c40b4d796fe
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-08-16 21:05:26 -0300

    add config for jwt and cookies

--- src/@types/fastify.d.ts
+++ src/@types/fastify.d.ts
@@ -1,11 +1,22 @@
-import fastify from "fastify";
-import { type userRole } from "../database/schema.ts";
+import { JWT } from "@fastify/jwt";
 
 declare module "fastify" {
-	export interface FastifyRequest {
-		user?: {
-			sub: string;
-			role: userRole;
-		};
+	interface FastifyRequest {
+		jwt: JWT;
+	}
+	export interface FastifyInstance {
+		authenticate: any;
+	}
+}
+
+type UserPayload = {
+	id: string;
+	email: string;
+	name: string;
+};
+
+declare module "@fastify/jwt" {
+	interface FastifyJWT {
+		user: UserPayload;
 	}
 }
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import { randomUUID } from "node:crypto";
 import {
 	TypeBoxTypeProvider,
@@ -7,6 +7,11 @@ import {
 import { fastifySwagger } from "@fastify/swagger";
 import scalarAPIReference from "@scalar/fastify-api-reference";
 import routes from "./routes/index.ts";
+import fjwt, { FastifyJWT } from "@fastify/jwt";
+import fCookie from "@fastify/cookie";
+
+const jwtSecret = String(process.env.JWT_SECRET);
+const cookieSecret = String(process.env.COOKIE_SECRET);
 
 const server = fastify({
 	genReqId: () => randomUUID(),
@@ -20,6 +25,7 @@ const server = fastify({
 		},
 	},
 }).withTypeProvider<TypeBoxTypeProvider>();
+server.setValidatorCompiler(TypeBoxValidatorCompiler);
 
 if (process.env.NODE_ENV === "development") {
 	server.register(fastifySwagger, {
@@ -36,8 +42,36 @@ if (process.env.NODE_ENV === "development") {
 	});
 }
 
-server.setValidatorCompiler(TypeBoxValidatorCompiler);
+server.register(fjwt, {
+	secret: jwtSecret,
+});
+
+server.decorate(
+	"authenticate",
+	async (req: FastifyRequest, reply: FastifyReply) => {
+		const token = req.cookies.access_token;
+
+		if (!token) {
+			return reply.status(401).send({
+				message: "Unauthorized",
+			});
+		}
+
+		const decoded = req.jwt.verify<FastifyJWT["user"]>(token);
+		req.user = decoded;
+	}
+);
 
 server.register(routes, { prefix: "/api" });
+
+server.addHook("preHandler", (req, reply, next) => {
+	req.jwt = server.jwt;
+	return next();
+});
+
+server.register(fCookie, {
+	secret: cookieSecret,
+	hook: "preHandler",
+});
 
 export { server };
